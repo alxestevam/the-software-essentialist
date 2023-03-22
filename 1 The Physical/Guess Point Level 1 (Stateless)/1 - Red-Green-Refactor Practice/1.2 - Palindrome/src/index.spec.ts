4854b11dc25dd4086c3f8fd49611f004f622d939
Alexandre Estevam <alexandre.estevam@going2.com.br>
2023-03-22 19:46:45 -0300

    refactor: move sut to beforeEach

--- 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.2 - Palindrome/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.2 - Palindrome/src/index.spec.ts
@@ -2,22 +2,26 @@ import { PalindromeChecker } from './index'
 
 
 describe('palindrome checker', () => {
+    let sut: PalindromeChecker;
+
+    beforeEach(() => {
+        sut = new PalindromeChecker();
+    })
+
     it('should return true if mom is an input', () => {
-        const sut = new PalindromeChecker()
         const output = sut.check('mom')
         expect(output).toEqual(true)
     })
 
     it('should return true if wow is an input', () => {
-        const sut = new PalindromeChecker()
         const output = sut.check('wow')
         expect(output).toEqual(true)
     })
 
     it('should return false if bill is an input', () => {
-        const sut = new PalindromeChecker()
         const output = sut.check('bill')
         expect(output).toEqual(false)
     })
+    
   
 })
