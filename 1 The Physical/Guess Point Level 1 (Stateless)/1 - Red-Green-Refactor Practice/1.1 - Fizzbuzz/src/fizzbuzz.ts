c65c4f97d2fbce8a03968ee3f84e06076e6bafe5
Alexandre Estevam <alexandre.estevam@going2.com.br>
2023-03-21 21:17:10 -0300

    feat: return Fizz for multiples of 3

--- 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/fizzbuzz.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/fizzbuzz.ts
@@ -1,3 +1,7 @@
 export function fizzBuzz(number: number) {
-    throw new Error('Outside of range 1 to 100')
+    if (number < 1 || number > 100) {
+        throw new Error('Outside of range 1 to 100')
+    }
+    
+    return 'Fizz'
 }
--- 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/index.spec.ts
@@ -4,6 +4,12 @@ import { fizzBuzz } from './fizzbuzz'
 describe("fizzbuzz", () => {
 
     it('should throw an error if the input is out of range 1 to 100', () => {
-        expect(() => fizzBuzz(0)).toThrow();      
+        expect(() => fizzBuzz(0)).toThrow()
+    })
+
+    it.each([3, 6, 9, 12])
+    ('should return Fizz for multiples of 3', (input: number) => {
+        const output = fizzBuzz(input)
+        expect(output).toEqual('Fizz')
     })
 });
