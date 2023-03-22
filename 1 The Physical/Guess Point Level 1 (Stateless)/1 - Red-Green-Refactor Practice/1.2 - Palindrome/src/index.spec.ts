eb0726a4cb4d53e40916a7a1c447f984166a7dbd
Alexandre Estevam <alexandre.estevam@going2.com.br>
2023-03-22 19:43:08 -0300

    feat: return true if mom is an input

--- 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.2 - Palindrome/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.2 - Palindrome/src/index.spec.ts
@@ -1,4 +1,11 @@
+import { PalindromeChecker } from './index'
+
 
 describe('palindrome checker', () => {
-
+    it('should return true if mom is an input', () => {
+        const sut = new PalindromeChecker()
+        const output = sut.check('mom')
+        expect(output).toEqual(true)
+    })
+  
 })
---
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.2 - Palindrome/src/index.ts
@@ -0,0 +1,5 @@
+export class PalindromeChecker {
+    check(string: string) {
+        return true
+    }
+}
