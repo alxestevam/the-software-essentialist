1352e4c179d6439407a5961a74b163a145d5ec9c
Alexandre Estevam <alexandre.estevam@going2.com.br>
2023-03-21 21:07:11 -0300

    feat: throw an error if the input is out of range 1 to 100

---
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/fizzbuzz.ts
@@ -0,0 +1,3 @@
+export function fizzBuzz(number: number) {
+    throw new Error('Outside of range 1 to 100')
+}
--- 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/index.spec.ts
@@ -1,5 +1,9 @@
 
+import { fizzBuzz } from './fizzbuzz'
+
 describe("fizzbuzz", () => {
 
-
+    it('should throw an error if the input is out of range 1 to 100', () => {
+        expect(() => fizzBuzz(0)).toThrow();      
+    })
 });
