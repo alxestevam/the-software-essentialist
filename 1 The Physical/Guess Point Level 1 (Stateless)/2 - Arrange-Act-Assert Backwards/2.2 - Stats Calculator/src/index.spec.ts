b1283babdb4d119ffccd0b635dd789c1e4685e9c
Alexandre Estevam <alexandre.estevam@going2.com.br>
2023-03-26 00:37:11 -0300

    feat: return the maximum value in the array

--- 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.2 - Stats Calculator/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.2 - Stats Calculator/src/index.spec.ts
@@ -11,4 +11,13 @@ describe('stats calculator', () => {
         expect(stats.min).toEqual(result)
     })
 
+    it.each([
+        [[2, 4, 21, -8, 53, 40], 53],
+        [[1, 2, 3, 4, 5, 6], 6],
+        [[3, 4, -2, 7, 10, 4], 10],
+    ])('should return the maximum value in the array', (input: number[], result: number) => {
+        const stats = calculateStats(input)
+        expect(stats.max).toEqual(result)
+    })
+
 })
--- 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.2 - Stats Calculator/src/index.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.2 - Stats Calculator/src/index.ts
@@ -2,11 +2,17 @@ function calculateMin(values: number[]) {
     return values.reduce((prev, cur) => prev < cur ? prev : cur, values[0])
 }
 
+function calculateMax(values: number[]) {
+    return values.reduce((prev, cur) => prev > cur ? prev : cur, values[0])
+}
+
 export function calculateStats(values: number[]) {
 
     const min = calculateMin(values)
+    const max = calculateMax(values)
 
     return {
-        min
+        min,
+        max
     }
 }
