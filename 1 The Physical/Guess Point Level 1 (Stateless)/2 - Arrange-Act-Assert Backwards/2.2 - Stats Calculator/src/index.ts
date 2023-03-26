8bc5b786fca9db5c6a93acbb04f0b019e0ca2f7d
Alexandre Estevam <alexandre.estevam@going2.com.br>
2023-03-26 00:35:24 -0300

    refactor: extract functions

--- 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.2 - Stats Calculator/src/index.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.2 - Stats Calculator/src/index.ts
@@ -1,12 +1,10 @@
+function calculateMin(values: number[]) {
+    return values.reduce((prev, cur) => prev < cur ? prev : cur, values[0])
+}
+
 export function calculateStats(values: number[]) {
 
-    const min = values.reduce((prev, cur) => {
-        if (prev < cur) {
-            return prev
-        } else {
-            return cur
-        }
-    }, values[0])
+    const min = calculateMin(values)
 
     return {
         min
