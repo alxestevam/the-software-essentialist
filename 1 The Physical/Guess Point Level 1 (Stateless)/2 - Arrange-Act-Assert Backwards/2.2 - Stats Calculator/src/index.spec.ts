3dcfc3b7e50bcbc0b32669a7cbe1ec5695a3f3f2
Alexandre Estevam <alexandre.estevam@going2.com.br>
2023-03-26 00:33:12 -0300

    feat: add more tests to ensure the minimum value is being returned from the array

--- 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.2 - Stats Calculator/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.2 - Stats Calculator/src/index.spec.ts
@@ -4,6 +4,8 @@ import { calculateStats } from './index'
 describe('stats calculator', () => {
     it.each([
         [[2, 4, 21, -8, 53, 40], -8],
+        [[1, 2, 3, 4, 5, 6], 1],
+        [[3, 4, -2, 7, 10, 4], -2],
     ])('should return the minimum value in the array', (input: number[], result: number) => {
         const stats = calculateStats(input)
         expect(stats.min).toEqual(result)
--- 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.2 - Stats Calculator/src/index.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.2 - Stats Calculator/src/index.ts
@@ -1,5 +1,14 @@
 export function calculateStats(values: number[]) {
+
+    const min = values.reduce((prev, cur) => {
+        if (prev < cur) {
+            return prev
+        } else {
+            return cur
+        }
+    }, values[0])
+
     return {
-        min: -8
+        min
     }
 }
