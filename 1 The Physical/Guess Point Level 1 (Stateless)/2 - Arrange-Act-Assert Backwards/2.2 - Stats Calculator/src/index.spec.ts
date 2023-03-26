7ef8d8e5782fca634ed3edb9bb7bab9b688112a3
Alexandre Estevam <alexandre.estevam@going2.com.br>
2023-03-26 00:28:11 -0300

    refactor: refactor test to be parameterized

--- 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.2 - Stats Calculator/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.2 - Stats Calculator/src/index.spec.ts
@@ -2,9 +2,11 @@
 import { calculateStats } from './index'
 
 describe('stats calculator', () => {
-    it('should return the minimum value in the array', () => {
-        const stats = calculateStats([2, 4, 21, -8, 53, 40])
-        expect(stats.min).toEqual(-8)
+    it.each([
+        [[2, 4, 21, -8, 53, 40], -8],
+    ])('should return the minimum value in the array', (input: number[], result: number) => {
+        const stats = calculateStats(input)
+        expect(stats.min).toEqual(result)
     })
 
 })
