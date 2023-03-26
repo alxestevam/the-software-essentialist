af33984284c2203ddd1482bf83124d5fcd99b5c5
Alexandre Estevam <alexandre.estevam@going2.com.br>
2023-03-26 00:25:27 -0300

    feat: return the minimum value in the array

--- 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.2 - Stats Calculator/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.2 - Stats Calculator/src/index.spec.ts
@@ -1,5 +1,10 @@
 
-describe('stats calculator', () => {
+import { calculateStats } from './index'
 
+describe('stats calculator', () => {
+    it('should return the minimum value in the array', () => {
+        const stats = calculateStats([2, 4, 21, -8, 53, 40])
+        expect(stats.min).toEqual(-8)
+    })
 
 })
---
+++ 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.2 - Stats Calculator/src/index.ts
@@ -0,0 +1,5 @@
+export function calculateStats(values: number[]) {
+    return {
+        min: -8
+    }
+}
